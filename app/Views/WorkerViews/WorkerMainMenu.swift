11865f64a2feae718ab24915a0a79deedfd926cf
Alexander Klopyshko <aklopyshko@gmail.com>
2021-01-11 00:15:12 +0300

    1

--- app/Views/WorkerViews/WorkerMainMenu.swift
+++ app/Views/WorkerViews/WorkerMainMenu.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct WorkerMainMenu: View {
     var body: some View {
         TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
-            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
-            Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+            Text("Страница 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+            Text("Страница 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
         }
         .navigationBarBackButtonHidden(true)
         
